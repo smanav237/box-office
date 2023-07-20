464a4c15168034dc3de3075606b3488127ad7d0a
Manav <manvashishth19@gmail.com>
2023-07-20 23:37:39 +0530

    React query pkg added to Home and Show

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter,Routes, Route } from "react-router-dom"
+import {QueryClient,QueryClientProvider} from '@tanstack/react-query'
 import Home from "./pages/Home"
 import Starred from "./pages/Starred"
 import HeadLayout from "./components/HeadLayout"
 import Show from "./pages/Show"
 
+// Create a client (from TANSTACK REACT QUERY PACKAGE)
+const queryClient = new QueryClient()    
 
 const App = () => {
   return (
+    <QueryClientProvider client={queryClient}>
     <BrowserRouter>
     <Routes>
     
@@ -21,6 +25,7 @@ const App = () => {
     
     </Routes>
   </BrowserRouter>
+  </QueryClientProvider>
   )
 }
 
--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,14 +1,33 @@
+//import { useEffect, useState } from 'react';
+
 import {useParams } from 'react-router-dom';
+//The useParams hook returns an object of key/value pairs of the dynamic params
+// from the current URL that were matched by the <Route path>
+
+import { useQuery } from '@tanstack/react-query';
+import { getShowById } from '../api/tvmaze';
+
 
 const Show = () => {
     const {showId} = useParams();
+    const {data: showData, error: showError} = useQuery({
+      queryKey: ['show',showId],
+      queryFn: ()=> getShowById(showId),
+    })
     
+    if(showError)
+    {
+      return <div>We have an error: {showError.message}</div>
+    }
+    if(showData)
+    {
+      return <div>Got Show data: {showData.name}</div>
+    }
 
   return (
     <div>
-        Show page for {showId}
+        Data is loading {showId}
     </div>
   )
 }
-
 export default Show
